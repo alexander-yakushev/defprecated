f2f81bfa06e4224f743d938b66eee95b09186888
Alexander Yakushev <alex@bytopia.org>
2014-11-28 16:45:56 +0100

    Update README

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject defprecated "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject defprecated "0.1.0"
+  :description "Deprecation in Clojure made easy"
+  :url "https://github.com/alexander-yakushev/defprecated"
   :license {:name "CC0"
             :url "http://creativecommons.org/about/cc0"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
