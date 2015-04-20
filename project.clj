266fd14a8b4ad34081d128aa5c287ba64e40996f
Alexander Yakushev <alex@bytopia.org>
2015-04-20 15:56:54 +0300

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject defprecated "0.1.2-SNAPSHOT"
+(defproject defprecated "0.1.2"
   :description "Deprecation in Clojure made easy"
   :url "https://github.com/alexander-yakushev/defprecated"
   :license {:name "CC0"
