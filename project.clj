7eead7b3a014b2152b6bb1fbf7f08bed7ae59870
Alexander Yakushev <alex@bytopia.org>
2015-04-21 21:20:17 +0300

    Add coverage and linting setup

--- project.clj
+++ project.clj
@@ -4,8 +4,12 @@
   :license {:name "CC0"
             :url "http://creativecommons.org/about/cc0"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [cloverage "1.0.4"]]
+                   :plugins [[lein-midje "3.1.3"]
+                             [jonase/eastwood "0.2.1"]]
+                   :eastwood {:namespaces [:source-paths]}
+                   :aliases {"coverage" ["run" "-m" "cloverage.coverage"]}}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
   :aliases {"test-all" ["do" ["check"] ["midje"]]})
